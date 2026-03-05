47273c2b084e8ad50d27500167fdcc07f4d6917d
HanJeongmok <wjdahe1845@gmail.com>
2026-03-05 14:18:23 +0900

    8강, 폼을 다시 보여줄 때 기존의 입력값들은 유지되도록하고 적절한 입력창에 포커싱

--- src/main/java/com/back/domain/poast/controller/PostController.java
+++ src/main/java/com/back/domain/poast/controller/PostController.java
@@ -18,7 +18,7 @@ public class PostController {
     @ResponseBody
     public String writeForm() {
 
-        return getWriteForm();
+        return getWriteForm("", "", "");
     }
 
     @PostMapping("/posts/write")
@@ -30,14 +30,14 @@ public class PostController {
             return """
                     <div style="color:red">제목을 입력해주세요.</div>
                     %s
-                    """.formatted(getWriteForm());
+                    """.formatted(getWriteForm(title, content, "title"));
         }
 
         if(content.isBlank()) {
             return """
                     <div style="color:red">내용을 입력해주세요.</div>
                     %s
-                    """.formatted(getWriteForm());
+                    """.formatted(getWriteForm(title, content, "content"));
         }
         //
 
@@ -46,16 +46,25 @@ public class PostController {
         return "%d번 글이 작성되었습니다.".formatted(post.getId());
     }
 
-    private String getWriteForm() {
+    private String getWriteForm(String title, String content, String errorFieldName) {
         return """
                 <form method="post" action="/posts/write">
-                  <input type="text" name="title">
+                  <input type="text" name="title" value="%s" autoFocus>
                   <br>
-                  <textarea name="content"></textarea>
+                  <textarea name="content">%s</textarea>
                   <br>
                   <input type="submit" value="작성">
                 </form>
-                """;
+                
+                <script>
+                    const errorFieldName = "%s";
+                
+                    if(errorFieldName.length > 0) {
+                        const form = document.querySelector("form");
+                        form[errorFieldName].focus();
+                    }
+                </script>
+                """.formatted(title, content, errorFieldName);
     }
 
 }
