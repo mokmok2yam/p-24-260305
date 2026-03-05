560fbfe63aabc6b04dfeb42693bb4b0aba9c9383
HanJeongmok <wjdahe1845@gmail.com>
2026-03-05 15:11:49 +0900

    10강, 액션 메서드의 입력값을 객체로 모아서 받기 == @ModelAttribute, @Valid 사용하면 @Validated 는 제거가능

--- src/main/java/com/back/domain/poast/controller/PostController.java
+++ src/main/java/com/back/domain/poast/controller/PostController.java
@@ -2,18 +2,18 @@ package com.back.domain.poast.controller;
 
 import com.back.domain.poast.entity.Post;
 import com.back.domain.poast.service.PostService;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
-@Validated
 public class PostController {
 
     private final PostService postService;
@@ -21,20 +21,27 @@ public class PostController {
     @GetMapping("/posts/write-form")
     @ResponseBody
     public String writeForm() {
+
         return getWriteForm("", "", "", "");
+    }
+
+    @AllArgsConstructor
+    public static class WriteRequestForm {
+        @Size(min=2, max=10)
+        @NotBlank
+        private String title;
+
+        @NotBlank
+        @Size(min=2, max=100)
+        private String content;
     }
 
     @PostMapping("/posts/write")
     @ResponseBody
-    public String write(
-            @Size(min = 2, max = 10)
-            @NotBlank
-            String title,
-            @NotBlank
-            @Size(min = 2, max = 100)
-            String content
-    ) {
-        Post post = postService.write(title, content);
+    public String write(@Valid WriteRequestForm form) {
+
+        Post post = postService.write(form.title, form.content);
+
         return "%d번 글이 작성되었습니다.".formatted(post.getId());
     }
 
