91df9acdd74f9bbae11ffce775c5d102f7b7851e
HanJeongmok <wjdahe1845@gmail.com>
2026-03-10 11:35:19 +0900

    49강, form th:href 때문에 안됐던 부분 th:action으로 고치고 컨트롤러 url 매핑 수정 후 정상 작동 확인

--- src/main/java/com/back/domain/post/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/post/controller/PostController.java
@@ -64,11 +64,12 @@ public class PostController {
         Post post = postService.findById(id).get();
         ModifyRequestForm modifyRequestForm = new ModifyRequestForm(post.getTitle(), post.getContent());
         model.addAttribute("form", modifyRequestForm);
+        model.addAttribute("post", post);
 
         return "modify";
     }
 
-    @PutMapping("/posts/{id}/modify")
+    @PutMapping("/posts/{id}")
     @Transactional
     public String modify(@PathVariable int id,
                          @Valid @ModelAttribute("form") ModifyRequestForm form,
@@ -82,7 +83,7 @@ public class PostController {
         return "redirect:/posts/%d".formatted(post.getId()); // GET요청
     }
 
-    @DeleteMapping("/posts/{id}/delete")
+    @DeleteMapping("/posts/{id}")
     public String delete(@PathVariable int id) {
         postService.deleteById(id);
         return "redirect:/posts";
